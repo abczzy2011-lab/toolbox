d78021bd5f14211a9452d276401816858205f398
abczzy2011-lab <17625100088@163.com>
2026-08-28 12:11:53 +0800

    v1.0.0: add chmod to formula (fix execute permission)

--- tool-box.rb
+++ tool-box.rb
@@ -4,39 +4,12 @@ class ToolBox < Formula
   desc "Offline web-based toolkit with 27 utilities"
   homepage "https://github.com/abczzy2011-lab/toolbox"
   url "https://github.com/abczzy2011-lab/toolbox/releases/download/v1.0.0/toolbox.zip"
-  version "1.0.0"
   sha256 "8f15c60510a5135a6aa28a75369e5abdb4b5123d9cdeb42c2e0b33c0c9540093"
   license "MIT"
 
-  depends_on "python@3"
-
   def install
-    prefix.install Dir["*"]
-    libexec.mkdir
-    libexec.install prefix/"tool-box"
+    libexec.install Dir["*"]
+    chmod 0755, libexec/"tool-box"
     bin.write_exec_script libexec/"tool-box"
-  end
-
-  service do
-    run [HOMEBREW_PREFIX/"opt/tool-box/bin/tool-box", "serve", "--no-browser"]
-    keep_alive true
-    working_dir HOMEBREW_PREFIX/"opt/tool-box"
-    run_type "Interactive"
-  end
-
-  def caveats
-    <<~CAVEATS
-      ToolBox installed!
-      Start:  brew services start tool-box
-      Visit:  http://localhost:8899
-      Mobile: tool-box ip
-      Stop:   brew services stop tool-box
-    CAVEATS
-  end
-
-  test do
-    system formula.bin/"tool-box", "ip"
-    output = shell_output("#{bin}/tool-box ip")
-    assert_match(%r{http://[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+:\d+}, output)
   end
 end
